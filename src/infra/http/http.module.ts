ab7201cf2490997d2295cfec0d4d755f0d9b0f58
Emerson Garrido <emersongarrido@hotmail.com.br>
2022-12-17 10:07:46 -0400

    crteate new erros is test jest

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { SendNotification } from '@application/use-cases/send-notification';
 import { DataBaseModule } from '@infra/database/database.module';
 import { NotificationsController } from '@infra/http/controllers/notifications.controller';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
+import { CountRecipientNotification } from '@application/use-cases/count-recipient-notifications';
+import { GetRecipientNotification } from '@application/use-cases/get-recipient-notification';
+import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notification';
 
 @Module({
   imports: [DataBaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotification,
+    GetRecipientNotification,
+    ReadNotification,
+    UnreadNotification,
+  ],
 })
 export class HttpModule {}
